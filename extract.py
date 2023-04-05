29537e643114adbc3987e3d20a8fbd3c70fc3e71
mmasias@Win11 <manuel@masiasweb.com>
2023-04-05 09:48:28 +0200

    string in md

--- extract.py
+++ extract.py
@@ -2,11 +2,12 @@ import glob
 
 for filename in glob.glob('**/*.uc', recursive=True):
     with open(filename, "r") as input_file:
-        with open(f"{filename}.txt", "w") as output_file:
+        with open(f"{filename}.md", "w") as output_file:
+            output_file.write("|Line|String|\n|-|-|\n")
             line_number = 1
             for line in input_file:
                 line = line.replace('\t','')
                 if line.startswith('say("') or line.count('= "') > 0 or line.count('item.say("'):
-                    output_file.write(f"Linea {line_number}: {line}")
+                    output_file.write(f"[{line_number}]|{line}")
                 line_number += 1
     print(f"{line_number} lineas en {filename}")
